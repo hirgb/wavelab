38fba486a4aa484635fa129be71f51e4522acb27
zhangkefei <707898401@qq.com>
2018-01-09 20:30:04 +0800

    optimize

--- wavelab/stockfilter.py
+++ wavelab/stockfilter.py
@@ -8,25 +8,33 @@ def filter(strategyid):
     result = cursor.fetchall()
     stocklist = [i[0] for i in result]
     finaldata = []
-    if strategyid == 2:
+    #fan tan xian feng
+    if strategyid == 11:
         while len(stocklist):
             query = "select bar from %s order by date desc limit 10" % stocklist[-1]
             cursor = db.sqlquery(query)
             result = cursor.fetchall()
             barlist = [float(i[0]) for i in result]
             if isNegetive(barlist) and barlist[0] > -0.02:
-                query = "select code, name from wave_stocklist where code = '%s'" % stocklist[-1]
-                cursor = db.sqlquery(query)
-                result = cursor.fetchone()
-                code, name = result[0], result[1]
-                query = "select date, close from %s order by date desc limit 60" % code
-                cursor = db.sqlquery(query)
-                tempdata = cursor.fetchall()
-                date = [i[0] for i in tempdata]
-                value = [i[1] for i in tempdata]
-                date.reverse()
-                value.reverse()
-                finaldata.append({"code":code, "name":name, "date":date, "value":value})
+                finaldata.append(getStockData(stocklist[-1]))
+            stocklist.pop()
+        return finaldata
+    #chuang xin gao
+    elif strategyid == 12:
+        while len(stocklist):
+            query = "select close from %s order by date desc limit 60" % stocklist[-1]
+            cursor = db.sqlquery(query)
+            result = cursor.fetchall()
+            closelist = [float(i[0]) for i in result]
+            if max(closelist) == closelist[0]:
+                finaldata.append(getStockData(stocklist[-1]))
+            stocklist.pop()
+        return finaldata
+    #lian xu shang zhang
+    elif strategyid == 13:
+        stocklist = getStockList("select code from wave_stocklist where riseday > 5")
+        while len(stocklist):
+            finaldata.append(getStockData(stocklist[-1]))
             stocklist.pop()
         return finaldata
     else:
@@ -40,3 +48,22 @@ def isNegetive(numlist):
             return False
     else:
         return True
+def getStockList(query):
+    cursor = db.sqlquery(query)
+    result = cursor.fetchall()
+    stocklist = [i[0] for i in result]
+    return stocklist
+
+def getStockData(stockcode):
+    query = "select code, name from wave_stocklist where code = '%s'" % stockcode
+    cursor = db.sqlquery(query)
+    result = cursor.fetchone()
+    code, name = result[0], result[1]
+    query = "select date, close from %s order by date desc limit 60" % code
+    cursor = db.sqlquery(query)
+    tempdata = cursor.fetchall()
+    date = [i[0] for i in tempdata]
+    value = [i[1] for i in tempdata]
+    date.reverse()
+    value.reverse()
+    return {"code":code, "name":name, "date":date, "value":value}
